9e3c2671c0d6a3a8ce8bd2ec2d8de9cb73016b30
shengxianye <635767825@qq.com>
2016-09-24 21:12:00 +0800

    增加了-h 功能，使用更友好

--- mysqlbinlog_analysis.py
+++ mysqlbinlog_analysis.py
@@ -2,13 +2,11 @@
 #!/usr/bin/python
 import re, hashlib, time, datetime
 import MySQLdb
-#Author: yeshengxian
-#Mail: 635767825@qq.com
-#Func: analysis mysql  binlog to redolog or undolog
-#Version: 1.0
-
-
-import sys
+#from elasticsearch import Elasticsearch
+#from elasticsearch import helpers
+#import requests, json
+import optparse
+import sys,os
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -17,17 +15,18 @@ def mysql_column_list(host,user,password):
     conn = MySQLdb.connect(host, user, password, 'information_schema', charset="utf8");
     query = "select TABLE_SCHEMA,TABLE_NAME,COLUMN_NAME,ORDINAL_POSITION from information_schema.COLUMNS where TABLE_SCHEMA not in ('test','information_schema','performance_schema') order by TABLE_SCHEMA,TABLE_NAME,ORDINAL_POSITION;"
     cursor = conn.cursor()
-    cursor.execute(query)
+    cursor.execute(query) 
     numrows = int(cursor.rowcount)
     for i in range(numrows):
         row = cursor.fetchone()
         database = row[0]
         table = row[1]
         column = row[2]
-        order = row[3]
+        order = row[3]    
         if database in column_dict:
-            if table in column_dict[database]:
+            if table in column_dict[database]:     #此处判断一定要对，太坑了
                 column_dict[database][table].update({ order : column })
+                #column_dict[database][table][order] = column
             else:
                 column_dict[database].update( { table : { order : column } })
         else:
@@ -41,16 +40,16 @@ def redo_sql(sql_action,column_dict,dbname,tablename,sql):
     column_num = len(column_dict[dbname][tablename])
     if sql_action == 'INSERT':
         sql = sql.replace("SET",'values(')
-        for i in range(int(column_num),0,-1):
-            if i == 1:
+        for i in range(int(column_num),0,-1):       #替换values的值
+            if i == 1:                          
                 sql = sql.replace('@'+str(i)+'=',"")
             else:
-                sql = sql.replace('@'+str(i)+'=',",")
+                sql = sql.replace('@'+str(i)+'=',",") 
         sql = sql + ");"
     if sql_action == 'UPDATE':
-        sql_set = sql.split('SET')[1]
-        sql_where = sql.split('WHERE')[1].split('SET')[0]
-        sql_head = sql.split('WHERE')[0]
+        sql_set = sql.split('SET')[1]   #获取set后的内容
+        sql_where = sql.split('WHERE')[1].split('SET')[0]  #获取where 和 set 之间的内容
+        sql_head = sql.split('WHERE')[0]    #获取update database.table 字符串
         for i in range(int(column_num),0,-1):
             if i == 1:
                 sql_set = sql_set.replace('@'+str(i) , "`"+column_dict[dbname][tablename][i]+"`")
@@ -76,16 +75,16 @@ def undo_sql(sql_action,column_dict,dbname,tablename,sql):
         sql = sql.replace("INSERT",'DELETE')
         sql = sql.replace("INTO","FROM")
         sql = sql.replace("SET","WHERE")
-        for i in range(int(column_num),0,-1):
+        for i in range(int(column_num),0,-1):       #替换values的值
             if i == 1:
                 sql = sql.replace('@'+str(i),"`"+column_dict[dbname][tablename][i]+"`")
             else:
                 sql = sql.replace('@'+str(i),"AND "+"`"+column_dict[dbname][tablename][i]+"`")
-        sql = sql + ";"
+        sql = sql + ";"  
     if sql_action == 'UPDATE':
-        sql_set = sql.split('SET')[1]
-        sql_where = sql.split('WHERE')[1].split('SET')[0]
-        sql_head = sql.split('WHERE')[0]
+        sql_set = sql.split('SET')[1]   #获取set后的内容
+        sql_where = sql.split('WHERE')[1].split('SET')[0]  #获取where 和 set 之间的内容
+        sql_head = sql.split('WHERE')[0]    #获取update database.table 字符串
         for i in range(int(column_num),0,-1):
             if i == 1:
                 sql_set = sql_set.replace('@'+str(i) , "`"+column_dict[dbname][tablename][i]+"`")
@@ -107,38 +106,38 @@ def undo_sql(sql_action,column_dict,dbname,tablename,sql):
     return sql
 
 
-def read_slow_log(log_file,target_log,file_type,column_dict):
+def read_bin_log(infile,outfile,mode,column_dict,filtertable,filterdml):
     regex_timestamp = re.compile('SET TIMESTAMP')
     sql_info = []
     sql = ''
     rsize = 1024 * 1024 * 10
-    regex_sql = re.compile("###")
-    regex_end = re.compile("COMMIT")
-    regex_float = re.compile(r"(\d+\.\d+)")
+    regex_sql = re.compile("###")              #匹配到update,insert,delete,alter,后期加工处理
+    regex_end = re.compile("COMMIT")           #匹配到commit
+    regex_float = re.compile(r"(\d+\.\d+)")    #匹配
     regex_table = re.compile("\`.*?\`")
-    regex_float = re.compile(r"(\(\d+)\)")
+    regex_float = re.compile(r"(\(\d+)\)")     #匹配(255) (65534) 等奇怪的问题
     p = 0
-    stdin_log = open(log_file, 'r')
-    stdout_log = open(target_log, 'a+')
+    stdin_log = open(infile, 'r')
+    stdout_log = open(outfile, 'a+')         #用于写入错误日志
     try:
         while True:
-            stdin_log.seek(p, )
-            lines = stdin_log.readlines(rsize)
+            stdin_log.seek(p, )             
+            lines = stdin_log.readlines(rsize)         #一次读取多行，大小为rsize
             p = stdin_log.tell()
-            if lines:
-                for line in lines:
-                    if line.strip():
-                        try:
+            if lines:                                  #如果有记录数rsize，那么遍历行
+                for line in lines:                      #遍历行rsize
+                    if line.strip():  
+                        try: 
                             line = line.encode('utf-8')
                         except:
                             line = line.decode('GB2312', 'ignore').encode('utf-8')
-                    if regex_timestamp.match(line):
+                    if regex_timestamp.match(line):                        #匹配到时间
                         sql_info.append(line.strip())
-                        time_stamp = sql_info[0].split('=')[1].split('/')[0]
+                        time_stamp = sql_info[0].split('=')[1].split('/')[0]       #1472298067
                         timeArray = time.localtime(int(time_stamp))
-                        date_time_raw = time.strftime("%Y%m%d %H%M%S", timeArray)
+                        date_time_raw = time.strftime("%Y%m%d %H%M%S", timeArray)  #20160827 194107
                     elif regex_sql.match(line):
-                        sql = sql + line.strip()
+                        sql = sql + line.strip()  
                     elif regex_end.match(line):
                         sql = sql.replace("###",'')
                         sql = regex_float.sub("", sql)
@@ -146,16 +145,34 @@ def read_slow_log(log_file,target_log,file_type,column_dict):
                         sql_action = sql_sp[0]
                         table_info = regex_table.findall(sql)
                         dbname = table_info[0]
-                        tablename = table_info[1]
-                        if file_type == "redo":
+                        tablename = table_info[1]     #`table1`
+                        tablename2 = tablename.replace("`",'')
+                        if mode == "redo":                #生成redo log
                             sql_redo =   redo_sql(sql_action,column_dict,dbname,tablename,sql)
-                            sql_redo = "###" +  date_time_raw + "\n" +sql_redo
-                            stdout_log.write(str(sql_redo) + '\n')
-                        if file_type == "undo":
+                            sql_redo = "###" +  date_time_raw + "\n"  +  sql_redo
+                            if filtertable == "ALL" and filterdml == "ALL":
+                                stdout_log.write(str(sql_redo) + '\n') 
+                            elif filtertable == tablename2 and filterdml == "ALL":
+                                stdout_log.write(str(sql_redo) + '\n') 
+                            elif filtertable == "ALL" and filterdml == sql_action:
+                                stdout_log.write(str(sql_redo) + '\n')
+                            elif filtertable == tablename2 and filterdml == sql_action:
+                                stdout_log.write(str(sql_redo) + '\n')
+                            else:
+                                pass
+                        if mode == "undo":                #生成undo log
                             sql_undo =   undo_sql(sql_action,column_dict,dbname,tablename,sql)
-                            sql_undo = "\"" + sql_undo + "\""
                             sql_undo = "###" +  date_time_raw + "\n" +sql_undo
-                            stdout_log.write(str(sql_undo) + '\n')
+                            if filtertable == "ALL" and filterdml == "ALL":
+                                stdout_log.write(str(sql_redo) + '\n')
+                            elif filtertable == tablename2 and filterdml == "ALL":
+                                stdout_log.write(str(sql_redo) + '\n')
+                            elif filtertable == "ALL" and filterdml == sql_action:
+                                stdout_log.write(str(sql_redo) + '\n')
+                            elif filtertable == tablename2 and filterdml == sql_action:
+                                stdout_log.write(str(sql_redo) + '\n')
+                            else:
+                                pass
                         sql = ''
                         sql_info =  []
                     else:
@@ -163,23 +180,58 @@ def read_slow_log(log_file,target_log,file_type,column_dict):
             if not lines:
                 break
     finally:
-        stdin_log.close()
-        stdout_log.close()
+        stdin_log.close() 
+        stdout_log.close()   
+ 
 
 
 
 def main():
-    host = 'localhost'  #变量1
-    user = 'root'       #变量2
-    password = '123456' #变量3
-    file_type = "redo"  #变量4
-    log_file = '/www/env/mysql/arch/5.sql' #变量5
-    target_log = '/www/env/mysql/arch/5.log' #变量6
+    usage = "usage:  --host localhost --user root --pawwrod 123456 --mode redo --infile /www/env/mysql/arch/4.sql  --outfile /www/env/mysql/arch/4.log --filtertable tablename --filterdml INSERT" 
+    parser = optparse.OptionParser(usage=usage)
+    parser.add_option('--host',type='string',dest='host',default='NULL',help='Connect to mysql host ')  #支持远程解析binlog
+    parser.add_option('--user',type='string',dest='user',default='NULL',help='User for login in')
+    parser.add_option('--password',type='string',dest='password',default='NULL',help='Password for login in')
+    parser.add_option('--mode',type='string',dest='mode',default='NULL',help='redo|undo,the sql you want to get')
+    parser.add_option('--infile',type='string',dest='infile',default='NULL',help='Source binlog file')
+    parser.add_option('--outfile',type='string',dest='outfile',default='NULL',help='Target sql file ')
+    parser.add_option('--filtertable',type='string',dest='filtertable',default='ALL',help='table1 ,filter the table1 you want')
+    parser.add_option('--filterdml',type='string',dest='filterdml',default='ALL',help='INSERT|UPDATE|DELETE,filter the type of sql')
+    (options, args) = parser.parse_args()
+    if options.host == "NULL":
+        command = "python %s -h" % (sys.argv[0])
+        os.system(command)
+        #print "please add args:e.g.--host localhost"
+        exit();
+    if options.user == "NULL":
+        print "please add args:e.g.--user root"
+        exit();
+    if options.password == "NULL":
+        print "please add args:e.g.--password 123456"
+        exit();
+    if options.mode == "NULL":
+        print "please add args:e.g.--mode redo"
+        exit();
+    if options.infile == "NULL":
+        print "please add args:e.g.--infile /www/env/mysql/arch/5.sql"
+        exit();
+    if options.outfile == "NULL":
+        print "please add args:e.g.--outfile /www/env/mysql/arch/5.log"
+        exit();
+    
+    host = options.host          #变量1
+    user = options.user          #变量2
+    password = options.password  #变量3
+    mode = options.mode          #变量4
+    infile = options.infile    #变量5
+    outfile = options.outfile  #变量6
+    filtertable = options.filtertable
+    filterdml = options.filterdml   
+ 
     rsize = 1024 * 1024 * 10
-
-
-    column_dict = mysql_column_list(host,user,password)
-    read_slow_log(log_file,target_log,file_type,column_dict)
+    column_dict = mysql_column_list(host,user,password)     #获取数据库字典
+    read_bin_log(infile,outfile,mode,column_dict,filtertable,filterdml)
 
 if __name__ == "__main__":
     main()
+
